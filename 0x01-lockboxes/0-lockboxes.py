306660fd32d094bf826d81b0ce2848ef3b909356
iradukundapaci <paifique@gmail.com>
2024-03-17 20:58:54 +0200

    error handling

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,43 +5,16 @@ Module to handle lockboxes
 from typing import List, Set
 
 
-def loopKeys(boxIndex: int, boxKeys: Set, allBoxes: List[List[int]]) -> None:
-    """
-    Function to store found keys in boxes
-
-    args:
-        boxIndex: index of the next box
-        boxKeys: set of found box keys
-        allBoxes: list of all boxes
-
-    return: None
-    """
-    if boxIndex in boxKeys:
-        return
-
-    boxKeys.add(boxIndex)
-
-    if len(allBoxes[boxIndex]) == 0:
-        boxKeys.add(0)
-        return
-
-    for i in allBoxes[boxIndex]:
-        loopKeys(i, boxKeys, allBoxes)
-
-
 def canUnlockAll(boxes: List[List[int]]) -> bool:
-    """
-    Function that verifies if given boxes are unlockable
+    total_boxes = len(boxes)
+    visited_boxes: Set[int] = set()
+    stack = [0]  # Start DFS from box 0
 
-    arg:
-        boxes: list of boxes
+    while stack:
+        current_box = stack.pop()
+        visited_boxes.add(current_box)
+        for key in boxes[current_box]:
+            if key not in visited_boxes and key < total_boxes:
+                stack.append(key)
 
-    return: True if box is unlockable
-    """
-    allBoxKeys: Set = set()
-
-    loopKeys(0, allBoxKeys, boxes)
-
-    if len(allBoxKeys) == len(boxes):
-        return True
-    return False
+    return len(visited_boxes) == total_boxes
