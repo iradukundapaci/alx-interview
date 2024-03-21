f6e10aed1557fd672b053f3918766d43152c8e1a
iradukundapaci <paifique@gmail.com>
2024-03-21 13:19:58 +0200

    editing function

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,6 +3,8 @@
 Minimum operations
 """
 
+primeFactors = __import__("primefactor").primeFactors
+
 
 def minOperations(n: int) -> int:
     """
@@ -15,15 +17,6 @@ def minOperations(n: int) -> int:
     """
     if n <= 1:
         return 0
-
-    prime_factors_sum = 0
-    divisor = 2
-
-    while n > 1:
-        if n % divisor == 0:
-            prime_factors_sum += divisor
-            n //= divisor
-        else:
-            divisor += 1
-
-    return prime_factors_sum
+    else:
+        prime_factors = primeFactors(n)
+        return sum(prime_factors)
