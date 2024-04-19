f3fac3301f081363b4473f7d0e0af62f16dae2d5
iradukundapaci <paifique@gmail.com>
2024-04-19 23:58:32 +0200

    adding promises

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,27 @@
 #!/usr/bin/node
 const request = require('request');
-const parameter = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
-request(
-  `https://swapi-api.alx-tools.com/api/films/${parameter}/`,
-  (error, _, body) => {
-    if (error) {
-      console.log(error);
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
     }
-    const characterList = JSON.parse(body).characters;
-    characterList.forEach((element) => {
-      request(element, (error, _, body) => {
-        if (error) {
-          console.log(error);
-        }
-        console.log(JSON.parse(body).name);
-      });
-    });
-  }
-);
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      (url) =>
+        new Promise((resolve, reject) => {
+          request(url, (promiseErr, __, charactersReqBody) => {
+            if (promiseErr) {
+              reject(promiseErr);
+            }
+            resolve(JSON.parse(charactersReqBody).name);
+          });
+        }),
+    );
+
+    Promise.all(charactersName)
+      .then((names) => console.log(names.join('\n')))
+      .catch((allErr) => console.log(allErr));
+  });
+}
