379efac38d70fe11a7e54791d7a224c7abba7d4d
iradukundapaci <paifique@gmail.com>
2024-04-24 21:17:12 +0200

    adding matrix rotation task

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,20 @@
+"""Rotate 2d matrix"""
+
+
+def rotate_2d_matrix(matrix) -> None:
+    """
+    Rotate 2d matrix 90 degrees clockwise
+
+    args:
+        matrix: matrix to rotate
+
+    return: none
+    """
+    matrix.reverse()
+
+    for row in range(len(matrix)):
+        for column in range(len(matrix)):
+            if row < column:
+                temp = matrix[row][column]
+                matrix[row][column] = matrix[column][row]
+                matrix[column][row] = temp
